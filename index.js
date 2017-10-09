2941f6d496693034434b35b3465ff46be1eba220
David Sánchez <david95sb@hotmail.com>
2017-10-09 18:34:53 -0500

    Add img parameter in  object platillo when create in database and get all objects platillo Integrate Bootstrap
    Signed-off-by: David Sánchez <david95sb@hotmail.com>

--- index.js
+++ index.js
@@ -11,32 +11,44 @@ var config = {
 firebase.initializeApp(config);
 
 /**
- * Database firebas
+ * Database firebas object
  * @private
  */
 var _database = firebase.database();
 
 /**
- * Storage firebase
+ * Storage firebase object
  * @private
  */
 var _storage = firebase.storage();
 
+/**
+ * Referencie  object storage firebase
+ * @private
+ */
 var _storageRef = _storage.ref();
+
+/**
+ * Referencie alimentos table
+ * @private
+ */
+var _refAlimentosTable = _database.ref( 'alimentos/' );
 
 /**
  * Add new platillo
  * @param name {string}
  * @param description {string}
  * @param price {number}
+ * @param imgDir {string}
  * @private
  */
-var _createPlatilloDatabase = function( name, description, price ){
+var _createPlatilloDatabase = function( name, description, price, imgDir ){
     _database.ref('alimentos').push({
         name: name,
         description: description,
         price: price,
-        cantidad: 0
+        cantidad: 0,
+        img : imgDir
     })
 };
 
@@ -44,10 +56,11 @@ var _createPlatilloDatabase = function( name, description, price ){
  * Create platillos
  */
 function addPlatillo() {
-    var _name = document.getElementById("name").value;
-    var _description = document.getElementById("description").value;
-    var _price = document.getElementById("price").value;
-    _createPlatilloDatabase( _name, _description, _price );
+    var _name = document.getElementById( "name" ).value;
+    var _description = document.getElementById( "description" ).value;
+    var _price = document.getElementById( "price" ).value;
+    var _imgDir = document.getElementById( "imgDir" ).value;
+    _createPlatilloDatabase( _name, _description, _price, _imgDir );
 }
 
 /**
@@ -62,7 +75,7 @@ function viewImg() {
     }
     if ( _file ) {
         _lector.readAsDataURL( _file );
-        var _putImg = _storageRef.child( "platillos/"+ _file.name ).put( _file );
+        var _putImg = _storageRef.child( "platillos/" + _file.name ).put( _file );
         _putImg.on( "state_changed", function ( snapshot ) {
             //change on load the file
             
@@ -72,8 +85,39 @@ function viewImg() {
         }, function success () {
             //success load file
             console.log( _putImg.snapshot.downloadURL );
+            document.getElementById( "imgDir" ).value = _putImg.snapshot.downloadURL;
         } )
     }else{
         _preview.src = "";
     }
+}
+
+/**
+ * Get platillos
+ */
+function getPlatillos() {
+    _refAlimentosTable.on( "value", function ( snapshot ) {
+        snapshot.forEach( function ( childSnapshot ) {
+            console.log( childSnapshot.val() );
+            var _ul = document.getElementById( "lista" );
+            var _childKey = childSnapshot.key;
+            var _childData = childSnapshot.val();
+            var _li = document.createElement( "li" );
+            var _div = document.createElement( "div" );
+            var _img = document.createElement( "img" );
+            var _br = document.createElement( "br" );
+
+            _img.src = _childData.img;
+            _img.height = 60;
+            _img.alt = "Imágen del platillo";
+
+            _div.appendChild( _img );
+            _li.appendChild( _div );
+            _li.appendChild( document.createTextNode( "Name: " + _childData.name ) );
+            _li.appendChild( document.createTextNode( "Description: " + _childData.description ) );
+            _li.appendChild( document.createTextNode( "Price: " + _childData.price ) );
+            _ul.appendChild( _li );
+        } );
+        console.log( snapshot.val() );
+    } );
 }
